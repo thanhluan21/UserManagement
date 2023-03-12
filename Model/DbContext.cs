1755a85b60afd074e5d18cc44658fd3d9459366a
ThaiThanhLuan <97227929+ThaiThanhLuan@users.noreply.github.com>
2023-03-12 23:55:49 +0700

    Create User mgt

---
+++ Model/DbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserManagement.Model
+{
+    public class DbContext : IdentityDbContext<IdentityUser>
+    {
+        public DbContext(DbContextOptions<DbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
