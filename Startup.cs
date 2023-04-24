122eccb90355651c0c8d381a4086353d0caf403f
ThaiThanhLuan <97227929+ThaiThanhLuan@users.noreply.github.com>
2023-04-24 15:37:15 +0700

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -33,23 +33,23 @@ namespace UserManagement
         // This method gets called by the runtime. Use this method to add services to the container
         public void ConfigureServices(IServiceCollection services)
         {
-            /* services.AddCors(options =>
-             {
-                 options.AddPolicy(name: "AllowAll",
-                     builder =>
-                     {
-                         builder.AllowAnyOrigin()
-                                .AllowAnyMethod()
-                                .AllowAnyHeader();
-                     });
-             });*/
             services.AddCors(options =>
+            {
+                options.AddPolicy(name: "AllowAll",
+                    builder =>
+                    {
+                        builder.AllowAnyOrigin()
+                               .AllowAnyMethod()
+                               .AllowAnyHeader();
+                    });
+            });
+            /*services.AddCors(options =>
             {
                 options.AddPolicy("AllowSpecificOrigin", builder =>
                     builder.WithOrigins("https://main.do70hhpzkq808.amplifyapp.com")
                            .AllowAnyHeader()
                            .AllowAnyMethod());
-            });
+            });*/
             services.AddTransient<IProduct, Product>();
             services.AddControllers().AddNewtonsoftJson();
             services.AddControllers();
@@ -130,8 +130,8 @@ namespace UserManagement
             {
                 app.UseDeveloperExceptionPage();
             }
-            /*            app.UseCors("AllowAll");
-            */
+            app.UseCors("AllowAll");
+
             app.UseCors("AllowSpecificOrigin");
             app.UseSwagger();
             app.UseSwaggerUI(c =>
